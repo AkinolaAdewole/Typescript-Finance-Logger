5b2f743a965928f73accd72d7d49bf59a5aaae1b
Akinola <adewoleakinola24@gmail.com>
2023-05-31 23:45:33 +0100

    Renderingg

--- public/App.js
+++ public/App.js
@@ -1,8 +1,24 @@
+import { Invoice } from "./classes/invoice.js";
+import { Payment } from "./classes/payment.js";
+import { listTemplate } from "./classes/listTemplate.js";
 const form = document.querySelector('.new-item-form');
+//Inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
 //list template
-const ul;
-export {};
+const ul = document.querySelector('ul');
+const list = new listTemplate(ul);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let doc;
+    if (type.value === 'Invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    list.render(doc, type.value, 'end');
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber, doc);
+});
--- src/App.ts
+++ src/App.ts
@@ -5,10 +5,34 @@ import { listTemplate } from "./classes/listTemplate.js";
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
+//Inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
 //list template
-const ul
+const ul= document.querySelector('ul')!;
+const list= new listTemplate(ul);
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    let doc:Hasformatter;
+
+    if (type.value==='Invoice'){
+     
+
+        doc=new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }else{
+        doc=new Payment(tofrom.value, details.value, amount.valueAsNumber)
+    }
+
+    list.render(doc, type.value, 'end');
+
+    console.log(
+        type.value, 
+        tofrom.value, 
+        details.value, 
+        amount.valueAsNumber,
+        doc);
+})
